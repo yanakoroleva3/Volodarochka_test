98c0d70a2d466d9c60288c51a5be8af92083a094
Yana <yanakoroleva2003@gmail.com>
2022-07-22 18:35:15 +0300

    Add JS for header__menu__more and fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
     });
   });
 
+  function myFunction() {
+    document.getElementById("header__more").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.menu_open')) {
+
+    let dropdowns = document.getElementsByClassName("header__menu__more__content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
